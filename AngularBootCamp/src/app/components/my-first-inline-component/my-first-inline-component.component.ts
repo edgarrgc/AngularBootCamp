14691b496220814431e8633202d59515418b4349
Edgar Gutierrez <Edgar.Gutierrez@accumatch.com>
2024-03-12 10:42:06 -0600

    modules

---
+++ AngularBootCamp/src/app/components/my-first-inline-component/my-first-inline-component.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first-inline-component',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      my-first-inline-component works!
+    </p>
+  `,
+  styles: ``
+})
+export class MyFirstInlineComponentComponent {
+
+}
